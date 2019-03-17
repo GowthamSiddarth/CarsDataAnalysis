62eb0708086e0de160ce951702b572cd60b123f7
Gowtham Behara <gothsiddu@gmail.com>
2019-03-17 15:42:22 +0530

    Iterate line by line of dataset

--- src/main/java/com/gowtham/cars/helper/ProcessData.java
+++ src/main/java/com/gowtham/cars/helper/ProcessData.java
@@ -1,12 +1,40 @@
 package com.gowtham.cars.helper;
 
 
-import java.io.File;
+import com.gowtham.cars.models.SortedCars;
+
+import java.io.*;
+import java.util.HashMap;
 
 public class ProcessData {
     private File dataset;
+    private boolean header;
 
-    public ProcessData(File dataset) {
+    public ProcessData(File dataset, boolean header) {
         this.dataset = dataset;
+        this.header = header;
+    }
+
+    public HashMap<String, SortedCars> getSortedCarsByOrigin() {
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(dataset));
+            if (header) {
+                bufferedReader.readLine();
+            }
+
+            String line = null;
+            HashMap<String, SortedCars> sortedCarsByOrigin = new HashMap<String, SortedCars>();
+            do {
+                line = bufferedReader.readLine();
+            } while (null != line);
+
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
