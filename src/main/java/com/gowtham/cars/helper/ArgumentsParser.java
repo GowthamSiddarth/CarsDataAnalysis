be9e192f13e42a97832f0cafeb9a93e885062669
Gowtham Behara <gothsiddu@gmail.com>
2019-03-17 14:49:20 +0530

    Define method to create list of options

--- src/main/java/com/gowtham/cars/helper/ArgumentsParser.java
+++ src/main/java/com/gowtham/cars/helper/ArgumentsParser.java
@@ -1,12 +1,36 @@
 package com.gowtham.cars.helper;
 
 
+import org.apache.commons.cli.*;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ArgumentsParser {
     private File file;
     private int numOfCars;
     private String origin;
+
+    private List<Option> getOptionsList() {
+        List<Option> options = new LinkedList<Option>();
+
+        Option filePathOption = new Option("f", "file", true, "Path to the dataset file");
+        filePathOption.setRequired(true);
+
+        Option numOfCarsOption = new Option("n", "numOfCars", true, "Number of Cars queried");
+        numOfCarsOption.setRequired(true);
+
+        Option originOption = new Option("o", "origin", true, "Origin of cars");
+        originOption.setRequired(true);
+
+        options.add(filePathOption);
+        options.add(numOfCarsOption);
+        options.add(originOption);
+
+        return options;
+    }
 
     public File getFilePath() {
         return file;
