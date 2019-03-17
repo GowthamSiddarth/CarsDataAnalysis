bd449f19d768601eec6fa82c5c2dd7413d918b7b
Gowtham Behara <gothsiddu@gmail.com>
2019-03-17 15:20:32 +0530

    Init cars set within constructor

--- src/main/java/com/gowtham/cars/models/SortedCars.java
+++ src/main/java/com/gowtham/cars/models/SortedCars.java
@@ -1,6 +1,8 @@
 package com.gowtham.cars.models;
 
+import java.util.Comparator;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SortedCars {
     private double averageHorsePower;
@@ -8,6 +10,11 @@ public class SortedCars {
 
     public SortedCars() {
         averageHorsePower = 0.0;
+        cars = new TreeSet<Car>(new Comparator<Car>() {
+            public int compare(Car car1, Car car2) {
+                return Double.compare(car1.getHorsePower() - car2.getHorsePower(), 0.0);
+            }
+        });
     }
 
     public boolean addCar(Car car) {
