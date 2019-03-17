20444ed1ba3ed40833bef10a7282751147c27466
Gowtham Behara <gothsiddu@gmail.com>
2019-03-17 15:29:24 +0530

    Create class for Processing Dataset

---
+++ src/main/java/com/gowtham/cars/helper/ProcessData.java
@@ -0,0 +1,12 @@
+package com.gowtham.cars.helper;
+
+
+import java.io.File;
+
+public class ProcessData {
+    private File dataset;
+
+    public ProcessData(File dataset) {
+        this.dataset = dataset;
+    }
+}
