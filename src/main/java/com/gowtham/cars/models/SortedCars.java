385ac401179dbde8c4038e4fa9a47d88a7bb4a05
Gowtham Behara <gothsiddu@gmail.com>
2019-03-17 15:14:22 +0530

    Create model for SortedCars

---
+++ src/main/java/com/gowtham/cars/models/SortedCars.java
@@ -0,0 +1,12 @@
+package com.gowtham.cars.models;
+
+import java.util.Set;
+
+public class SortedCars {
+    private double averageHorsePower;
+    private Set<Car> cars;
+
+    public SortedCars() {
+        averageHorsePower = 0.0;
+    }
+}
